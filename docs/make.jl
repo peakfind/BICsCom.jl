2d2cc4db89be6574796468977d67b6563a05d980
jyzhang <peakfind@126.com>
2026-08-18 17:08:18 +0800

    doc: generate docs for functions in CMakieExt

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,15 @@
 using BICsCom
 using Documenter
 
+# for CMakieExt
+using CairoMakie
+
 DocMeta.setdocmeta!(BICsCom, :DocTestSetup, :(using BICsCom); recursive=true)
 
+const cmakieExt = Base.get_extension(BICsCom, :CMakieExt)
+
 makedocs(;
-    modules=[BICsCom],
+    modules=[BICsCom, cmakieExt],
     authors="Jiayi Zhang",
     sitename="BICsCom.jl",
     format=Documenter.HTML(;
@@ -14,6 +19,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "API" => "api.md"
     ],
 )
 
---
+++ docs/src/api.md
@@ -0,0 +1,11 @@
+# API
+
+## Spherical expansion
+
+## Extension: CMakieExt
+
+```@docs
+CMakieExt.plot_square!
+CMakieExt.plot_samplingpoints!
+CMakieExt.plot_min_svals!
+```
