ccd0547fe75037e16e5d8c7f032e42e6f0f68381
jyzhang <peakfind@126.com>
2026-09-08 17:21:27 +0800

    src: add ModeField, compute_mode(), evaluate_field()

--- src/BICsCom.jl
+++ src/BICsCom.jl
@@ -1,6 +1,5 @@
 module BICsCom
 
-using Printf
 using LinearAlgebra
 using Bessels
 
@@ -16,9 +15,9 @@ export apply_bc
 export beta_m, assemble_tbc, apply_tbc!
 
 include("mode.jl")
-export compute_mode, eval_field
+export ModeField, compute_mode, evaluate_field
 
 include("ext.jl")
-export plot_square!, plot_samplingpoints!, plot_min_svals!
+export plot_square!, plot_samplingpoints!, plot_min_svals!, plot_field_mode!
 
 end
--- src/mode.jl
+++ src/mode.jl
@@ -1,7 +1,41 @@
 # functions for compute fields of modes corresponding to BICs
 
+"""
+    ModeField
+
+Contain information of modes corresponding to BICs. 
+
+# Fields
+- `k::Float64`: the wavenumber
+- `inn::Float64`: the dielectric constant inside the cylinders or air-holes
+- `ext::Float64`: the dielectric constant outside the cylinders or air-holes
+- `r::Float64`: the radius of the cylinders or air-holes
+- `orders::Vector{Int64}`: the orders of Bessel functions and Neumann functions
+- `coeffs::Matrix{Float64}`: the coefficients in the spherical expansion outside the cylinder, see [`get_coeff`](@ref)
+- `expan_coeffs::Vector{ComplexF64}`: the coefficients before special functions
+"""
+struct ModeField 
+    k::Float64 
+    inn::Float64
+    ext::Float64
+    r::Float64
+    orders::Vector{Int64}
+    coeffs::Matrix{Float64}
+    expan_coeffs::Vector{ComplexF64}
+end
+
+"""
+    compute_mode(sp::SamplingPoints, k, r, inn, ext, homo)
+
+TBW
+
+# Arguments
+"""
 function compute_mode(sp::SamplingPoints, k, r, inn, ext, homo)
     n = sp.n
+    
+    odrs = [j - 1 - 2n for j in 1:4n]
+
     cydc = build_cylinder_cache(4n, k, r, inn, ext)
     coef = get_coeff(4n, cydc, inn, ext)
     bydc = build_boundary_cache(sp, 4n, k, ext)
@@ -13,8 +47,6 @@ function compute_mode(sp::SamplingPoints, k, r, inn, ext, homo)
     idx = argmin(abs.(evals))
     v = evecs[:, idx]
     
-    @printf("k = %.4f, |λ_bic| = %.2e\n", k, abs(evals[idx]))
-    
     # Reconstruct the Dirichlet data on 4 edges
     vb = v[1:n]
     vl = v[n+1:2n]
@@ -23,40 +55,40 @@ function compute_mode(sp::SamplingPoints, k, r, inn, ext, homo)
     u = [vb; vl; vt; vr]
     
     c = A \ u
-    
-    return c
+
+    return ModeField(k, inn, ext, r, odrs, coef, c)
 end
 
-function eval_field(xs, ys, k, r, inn, ext, dv, trunc, coeff)
-     field = zeros(ComplexF64, length(ys), length(xs))
-    ki = k * sqrt(inn)
-    ke = k * sqrt(ext)
+function evaluate_field(mf::ModeField, xs, ys)
+    field = zeros(ComplexF64, length(xs), length(ys))
+    ki = mf.k * sqrt(mf.inn)
+    ke = mf.k * sqrt(mf.ext)
+    trunc = length(mf.orders)
     
-    orders = [j - 1 - trunc ÷ 2 for j in 1:trunc]
-
-    for i in eachindex(xs), j in eachindex(ys)
-        x = xs[i]
+    @inbounds for j in eachindex(ys)
         y = ys[j]
-        rd = sqrt(x^2 + y^2)
-        θ = atan(y, x)
+
+        for i in eachindex(xs)
+            x = xs[i]
+            rd = sqrt(x^2 + y^2)
+            θ = atan(y, x)
         
-        val = zero(ComplexF64)
+            val = zero(ComplexF64)
+
+            if rd < mf.r 
+                for t in 1:trunc 
+                    val += mf.expan_coeffs[t] * besselj(mf.orders[t], ki * rd) * exp(im * mf.orders[t] * θ)
+                end
+            else
+                for t in 1:trunc
+                    radial = mf.coeffs[1, t] * besselj(mf.orders[t], ke * rd) + mf.coeffs[2, t] * bessely(mf.orders[t], ke * rd)
+                    val += mf.expan_coeffs[t] * radial * exp(im * mf.orders[t] * θ)
+                end
+            end
         
-        if rd < r 
-            for t in 1:trunc 
-                val += dv[t] * besselj(orders[t], ki * rd) * exp(im * orders[t] * θ)
-            end
-        else
-            for t in 1:trunc
-                radial = coeff[1, t] * besselj(orders[t], ke * rd) +
-                         coeff[2, t] * bessely(orders[t], ke * rd)
-                val += dv[t] * radial * exp(im * orders[t] * θ)
-            end
+            field[i, j] = val
         end
-        
-        field[j, i] = val
-        
     end
-    
+
     return field
 end
